8ef96e972ccd609805df0a0de4e8d52ae78dc317
Leo Lobato <leo@cliqconsulting.com>
2012-12-02 11:37:33 -0200

    Demo target

---
+++ AFKitDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AFKitDemo
+//
+//  Created by Leonardo Lobato on 12/2/12.
+//  Copyright (c) 2012 Cliq Consulting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AFKitDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AFKitDemo
+//
+//  Created by Leonardo Lobato on 12/2/12.
+//  Copyright (c) 2012 Cliq Consulting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
