e4f0dbc82598c5649a4a8547dce074a02d7a6a43
Leo Lobato <leo@cliqconsulting.com>
2012-12-02 10:49:44 -0200

    Initial Commit

---
+++ AFKit/AFKit.h
@@ -0,0 +1,13 @@
+//
+//  AFKit.h
+//  AFKit
+//
+//  Created by Leonardo Lobato on 12/2/12.
+//  Copyright (c) 2012 Cliq Consulting. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AFKit : NSObject
+
+@end
---
+++ AFKit/AFKit.m
@@ -0,0 +1,13 @@
+//
+//  AFKit.m
+//  AFKit
+//
+//  Created by Leonardo Lobato on 12/2/12.
+//  Copyright (c) 2012 Cliq Consulting. All rights reserved.
+//
+
+#import "AFKit.h"
+
+@implementation AFKit
+
+@end
