c8c8a3fc7abd4d70145e929bd2f27bba4054bd39
Leo Lobato <leo@cliqconsulting.com>
2013-08-13 11:53:06 -0300

    UIColor RGB values convenience methods

--- CCKit/CCDefines.h
+++ CCKit/CCDefines.h
@@ -54,6 +54,8 @@ return shared;                                  \
 
 #pragma mark - Utils
 
+#define CCRGBColor(r, g, b) [UIColor colorWithRed:r/255.0f green:g/255.0f blue:b/255.0f alpha:1.0f]
+#define CCRGBAColor(r, g, b, a) [UIColor colorWithRed:r/255.0f green:g/255.0f blue:b/255.0f alpha:a]
 #define CCHexColor(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 
 #pragma mark - Constants
